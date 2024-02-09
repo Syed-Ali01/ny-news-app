6eaecc6b036fc0b97fe721e849a5038a4e0e7015
Syed Ali <syed.ali01@nagarro.com>
2024-02-09 15:06:17 +0530

    updated ny app for mobile

--- src/component/Articles/__test__/Articles.test.jsx
+++ src/component/Articles/__test__/Articles.test.jsx
@@ -9,8 +9,26 @@ import "@testing-library/jest-dom";
 import "@testing-library/dom";
 import Articles from "..";
 import * as api from "../../../api/apiSlice";
+import mediaQuery from "css-mediaquery";
 
 jest.mock("../../../api/apiSlice");
+
+function createMatchMedia(width) {
+  return (query) => {
+    return {
+      matches: mediaQuery.match(query, { width }),
+      media: "",
+      onchange: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => true,
+    };
+  };
+}
+
+function resizeScreenSize(width) {
+  window.matchMedia = createMatchMedia(width);
+}
 
 describe("Article page", () => {
   beforeEach(() => jest.clearAllMocks());
@@ -96,6 +114,90 @@ describe("Article page", () => {
         "For First Time in Two Decades, U.S. Buys More From Mexico Than China"
       )
     );
+  });
+  it("render Article component in mobile", async () => {
+    resizeScreenSize(480);
+    api.fetchArticles.mockResolvedValue({
+      results: [
+        {
+          uri: "nyt://article/b04a8df5-1f93-5910-b484-ab852443e662",
+          url: "https://www.nytimes.com/2024/02/07/business/economy/united-states-china-mexico-trade.html",
+          id: 100000009292387,
+          asset_id: 100000009292387,
+          source: "New York Times",
+          published_date: "2024-02-07",
+          updated: "2024-02-07 09:47:38",
+          section: "Business",
+          subsection: "Economy",
+          nytdsection: "business",
+          adx_keywords:
+            "International Trade and World Market;United States Economy;Factories and Manufacturing;Supply Chain;Customs (Tariff);United States Politics and Government;United States International Relations;Russian Invasion of Ukraine (2022);Schneider Electric SA;China;Mexico;United States",
+          column: null,
+          byline: "By Ana Swanson and Simon Romero",
+          type: "Article",
+          title:
+            "For First Time in Two Decades, U.S. Buys More From Mexico Than China",
+          abstract:
+            "The United States bought more goods from Mexico than China in 2023 for the first time in 20 years, evidence of how much global trade patterns have shifted.",
+          des_facet: [
+            "International Trade and World Market",
+            "United States Economy",
+            "Factories and Manufacturing",
+            "Supply Chain",
+            "Customs (Tariff)",
+            "United States Politics and Government",
+            "United States International Relations",
+            "Russian Invasion of Ukraine (2022)",
+          ],
+          org_facet: ["Schneider Electric SA"],
+          per_facet: [],
+          geo_facet: ["China", "Mexico", "United States"],
+          media: [
+            {
+              type: "image",
+              subtype: "photo",
+              caption:
+                "A factory in the northern Mexico industrial hub of Saltillo. Mexico was among the markets that American consumers and businesses turned to last year for car parts, shoes, toys and raw materials.",
+              copyright: "Daniel Becerril/Reuters",
+              approved_for_syndication: 1,
+              "media-metadata": [
+                {
+                  url: "https://static01.nyt.com/images/2024/02/07/multimedia/07dc-trade-01-jwpg/07dc-trade-01-jwpg-thumbStandard.jpg",
+                  format: "Standard Thumbnail",
+                  height: 75,
+                  width: 75,
+                },
+                {
+                  url: "https://static01.nyt.com/images/2024/02/07/multimedia/07dc-trade-01-jwpg/07dc-trade-01-jwpg-mediumThreeByTwo210.jpg",
+                  format: "mediumThreeByTwo210",
+                  height: 140,
+                  width: 210,
+                },
+                {
+                  url: "https://static01.nyt.com/images/2024/02/07/multimedia/07dc-trade-01-jwpg/07dc-trade-01-jwpg-mediumThreeByTwo440.jpg",
+                  format: "mediumThreeByTwo440",
+                  height: 293,
+                  width: 440,
+                },
+              ],
+            },
+          ],
+          eta_id: 0,
+        },
+      ],
+    });
+    render(<Articles />);
+    await waitFor(() => {
+      screen.getByText(
+        "For First Time in Two Decades, U.S. Buys More From Mexico Than China"
+      );
+    });
+    fireEvent.click(
+      screen.getByText(
+        "For First Time in Two Decades, U.S. Buys More From Mexico Than China"
+      )
+    );
+    fireEvent.click(screen.getByRole("button"));
   });
   it("render Article component with failed api", async () => {
     api.fetchArticles.mockRejectedValue({});
--- src/component/Articles/index.jsx
+++ src/component/Articles/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, lazy, Suspense } from "react";
 import "./styles.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
 import { fetchArticles } from "../../api/apiSlice";
 const ArticleSection = lazy(() => import("../../common/ArticleSection"));
 const ArticleDetails = lazy(() => import("../../common/ArticleDetailSection"));
@@ -8,8 +9,13 @@ const Articles = () => {
   const [articles, setArticles] = useState([]);
   const [selectedArticles, setSelectedArticles] = useState({});
   const [errorToast, setErrorToast] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [showMobileArticleDetail, setShowMobileArticleDetail] = useState(false);
 
   useEffect(() => {
+    const checkForMobile = () =>
+      window.matchMedia && window.matchMedia("(max-width: 480px)").matches;
+    setIsMobile(checkForMobile());
     fetchArticles()
       .then((data) => {
         setArticles(data?.results);
@@ -22,27 +28,47 @@ const Articles = () => {
 
   const handleCallback = (data) => {
     setSelectedArticles(data);
+    if (isMobile) {
+      setShowMobileArticleDetail(true);
+    }
+  };
+
+  const handleBack = () => {
+    setShowMobileArticleDetail(false);
   };
   return (
     <>
       <section className="article-container p-4">
         <Suspense fallback={<p>Loading...</p>}>
-          <div>
-            <ArticleSection
-              articleData={articles}
-              callback={handleCallback}
-              selectedArticles={selectedArticles}
-              className={"articles-parent"}
-            />
-          </div>
-          <div>
-            {selectedArticles?.id && (
+          {!showMobileArticleDetail && (
+            <div>
+              <ArticleSection
+                articleData={articles}
+                callback={handleCallback}
+                selectedArticles={selectedArticles}
+                className={"articles-parent"}
+              />
+            </div>
+          )}
+          {!isMobile && (
+            <div>
+              {selectedArticles?.id && (
+                <ArticleDetails
+                  selectedArticles={selectedArticles}
+                  className={"article-detail"}
+                />
+              )}
+            </div>
+          )}
+          {isMobile && showMobileArticleDetail && (
+            <div>
+              <IoMdArrowRoundBack role="button" onClick={handleBack} />
               <ArticleDetails
                 selectedArticles={selectedArticles}
                 className={"article-detail"}
               />
-            )}
-          </div>
+            </div>
+          )}
         </Suspense>
       </section>
       {errorToast && <p className="error-toast">Article Api failed...</p>}
